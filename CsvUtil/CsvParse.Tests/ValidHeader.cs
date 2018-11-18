482ccfff7a733ac6ec2f98e1c57c47f0748d284d
Mike Pastore <ml.pastore@outlook.com>
2018-11-17 19:25:29 -0500

    parsing tests
    file headers

---
+++ CsvUtil/CsvParse.Tests/ValidHeader.cs
@@ -0,0 +1,67 @@
+using System;
+using Xunit;
+using System.IO;
+
+namespace CsvParse.Tests
+{
+    public class VadlidHeader
+    {
+
+        [Fact]
+        public void AssertRawFileHeaders()
+        {
+
+            HeadAssert ha = new HeadAssert{Fname = "GermanEnglishDict_nouns.csv"
+                , pFile = new ParseFileNoun()
+                , FType = "** NOUN **"};
+
+            string fRow = ha.FirstLine();
+            Assert.True(fRow == ha.pFile.FileRawHeader, $"Invalid raw {ha.FType} \nHeader: {fRow}\nExpected: {ha.ExpectedHeader()}");
+
+            ha.Fname = "GermanEnglishDict_adj.csv";
+            ha.FType = " ** ADJ ** ";
+            ha.pFile = new ParseFileAdj();
+            fRow = ha.FirstLine();
+            Assert.True(fRow == ha.pFile.FileRawHeader, $"Invalid raw {ha.FType} \nHeader: {fRow}\nExpected: {ha.ExpectedHeader()}");
+
+        }
+
+        public class HeadAssert{
+            public string Fname = string.Empty;
+            public ParseFile pFile ;
+            public string FType = string.Empty;
+            public string relPath = @"../../../../../";
+
+            public string ExpectedHeader()
+            {
+                return pFile.FileRawHeader;
+            }
+
+            public string FirstLine()
+            {
+                return GetFirstLine(relPath + Fname);
+
+            }
+
+            string GetFirstLine(string fName)
+            {
+
+                string absName = Path.GetFullPath(fName);
+            
+                string ret = "";
+                using (StreamReader reader = new StreamReader(absName)) {
+                    ret = reader.ReadLine();
+            }
+
+                return ret;
+
+            }
+
+        }
+      
+
+      
+        
+    }
+      
+}
